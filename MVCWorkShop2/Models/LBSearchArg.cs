224a4d2c8103cecd2f3ea26aa0fc53aec968172e
Lee szu-hsuan <sally20309@gmail.com>
2019-06-11 01:39:34 +0800

    套入kendoUI

---
+++ MVCWorkShop2/Models/LBSearchArg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVCWorkShop2.Models
+{
+    public class LBSearchArg
+    {
+ 
+        [DisplayName("書名")]
+        public string BookId { get; set; }
+        [DisplayName("書籍類別")]
+        public string BookClassName { get; set; }
+        [DisplayName("借閱人")]
+        public string BookKeeper { get; set; }
+        [DisplayName("借閱狀態")]
+        public string BookStatus { get; set; }
+    }
+}
